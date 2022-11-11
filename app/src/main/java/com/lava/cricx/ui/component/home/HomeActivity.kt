6709b5a25435c36f32c00d4da35fad98f4419cf5
Snehil <cascadenienuo@gmail.com>
2022-11-11 22:12:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/lava/cricx/ui/component/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.lava.cricx.ui.component.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lava.cricx.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
