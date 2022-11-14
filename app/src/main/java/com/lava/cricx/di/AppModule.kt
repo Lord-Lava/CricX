13655a1e5dc86d8608a5fa64c69d037fc05493b8
Snehil <cascadenienuo@gmail.com>
2022-11-14 11:46:37 +0530

    Add testing libraries in gradle, utils & network boilerplate

--- app/src/main/java/com/lava/cricx/di/AppModule.kt
+++ app/src/main/java/com/lava/cricx/di/AppModule.kt
@@ -1,11 +1,22 @@
 package com.lava.cricx.di
 
+import android.content.Context
+import com.lava.cricx.util.NetworkConnectivity
+import com.lava.cricx.util.NetworkManager
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideNetworkConnectivity(@ApplicationContext context: Context): NetworkConnectivity {
+        return NetworkManager(context)
+    }
 }
